a858220733dc93277cd589c01ea5ea4a4d83cbf7
fl1ss <felicity_emery@biswashington.org>
2025-01-30 17:51:07 +0000

    not complete

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+rps = input("Rock","Paper","Scissors")
+from random import *
+selections = ["Rock","Paper","Scissors"]
+computerChoice = selections[randint(0,2)]
+userChoice = input("Choose rock, paper or scissors")
+
+if (choice == "paper") and  (choice == "scissors"):
+    print = ("Well done user_1")
+computerChoice = int(input("Rock and paper: "))
+if Choice =
