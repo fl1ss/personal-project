731d700af966bcedf44d210403b6a68b83ab0955
fl1ss <felicity_emery@biswashington.org>
2025-02-06 10:00:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ userChoice = input("Choose rock, paper or scissors")
 if (choice == "paper") and  (choice == "scissors"):
     print = ("Well done user_1")
 computerChoice = int(input("Rock and paper: "))
-if Choice =
+if def determine_winner(user_choice, computer_choice):
+    """Determine the winner of the game."""
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == "rock"and computer_choice == '"scissors") or 
+         (user_choice == "scissors" and computer_choice == "paper") or 
+         (user_choice == "paper" and computer_choice == "rock"):
+        return "You win!"
+    else:
+        return "You lose!"
